42e49d129efb51ba027e647eafab95911ee9c460
Hagen Paul Pfeifer <hagen@jauu.net>
2017-01-30 22:24:45 +0100

    add more flexible execution routine
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- command-control-rest-daemon.py
+++ command-control-rest-daemon.py
@@ -45,11 +45,30 @@ def msg(msg):
     sys.stdout.write(msg)
 
 
-def execute_command(command):
-    print(command)
-    return subprocess.check_output(command.split(), shell=True).decode("utf-8") 
 
-async def handle_raw(request):
+def execute_command(cmd):
+    print("execute: {}".format(cmd))
+    use_shell = False
+    if 'shell' in cmd and cmd['shell'] == True:
+        use_shell = True
+    process = subprocess.Popen(cmd['cmd'].split(), shell=use_shell)
+    if 'wait' in cmd and cmd['wait'] == True:
+        process.wait()
+
+
+
+def execute_commands(commands):
+    for cmd in commands['commands']:
+        if "sleep-before" in cmd:
+            time.sleep(float(cmd["sleep-before"]))
+        execute_command(cmd)
+        if "sleep-after" in cmd:
+            time.sleep(float(cmd["sleep-after"]))
+    return 'ok'
+
+
+async def handle_exec(request):
+    loop = request.app['LOOP']
     try:
         request_data = await request.json()
     except json.decoder.JSONDecodeError:
@@ -57,18 +76,22 @@ async def handle_raw(request):
         body = json.dumps(response_data).encode('utf-8')
         return aiohttp.web.Response(body=body, content_type="application/json")
 
-    ret = list()
-    for cmd in request_data:
-        ret.append(execute_command(cmd))
 
-    response_data = {'status': 'ok', "data" : ret}
+    if "detach" in request_data and request_data['detach'] == True:
+        status = 'ok'
+        loop.call_soon(functools.partial(execute_commands, request_data))
+    else:
+        status = execute_commands(request_data)
+
+    response_data = { 'status': status }
     body = json.dumps(response_data).encode('utf-8')
     return aiohttp.web.Response(body=body, content_type="application/json")
 
 
 def http_init(loop):
     app = aiohttp.web.Application(loop=loop)
-    app.router.add_route('POST', "/v1/raw/", handle_raw)
+    app['LOOP'] = loop
+    app.router.add_route('POST', "/api/v1/exec", handle_exec)
     server = loop.create_server(app.make_handler(),
                                 conf.common.v4_listen_addr,
                                 conf.common.v4_listen_port)
